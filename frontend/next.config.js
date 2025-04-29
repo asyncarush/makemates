6bfeee59692779017ecf1be8f3fcea7d6d799179
asyncarush <arush3339@gmail.com>
2025-04-29 20:54:33 +0530

    fix image upload issue

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -16,7 +16,7 @@ const nextConfig = {
         port: "9000",
       },
       {
-        protocol: "http",
+        protocol: "http",   
         hostname: "104.197.136.197",
         port: "31927",
       },
